c4e11b59dcfb9c40141a5446c426028943fe6e42
manwithaplan-pepsiman@protonmail.com <manwithaplan-pepsiman@protonmail.com>
2022-07-31 22:26:15 -0500

    Added lines to circles

--- eulerianpath/main.py
+++ eulerianpath/main.py
@@ -1,9 +1,27 @@
+import random
+
 # Import and initialize pygame
 import pygame
 pygame.init()
 
 # Create the window (640x480)
 screen = pygame.display.set_mode([640, 480])
+
+# Create the circle class
+class Circle:
+    def __init__(self, position):
+        # Initialize the values
+        # Color will be random for each circle
+        # Position will be based off of mouse press
+        self.color = ((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        self.position = position
+    
+    def drawCircle(self):
+        # Draw the circle to the screen
+        pygame.draw.circle(screen, self.color, self.position, 15)
+
+# Number of circles
+onScreenCircles = []
 
 # The Main Loop
 while (1):
@@ -15,12 +33,19 @@ while (1):
             if event.key == pygame.K_ESCAPE:
                 quit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.draw.circle(screen, (0, 0, 255), pygame.mouse.get_pos(), 15)
+            # Create an instance of the currentCircle and append it to the onScreenSircles list to keep track of all circles
+            currentCircle = Circle(pygame.mouse.get_pos())
+            currentCircle.drawCircle()
+            onScreenCircles.append(currentCircle.position)
+
+            # Detect if more than one circle is on screen, if so draw a line to it, and the last one created
+            if (len(onScreenCircles) > 1):
+                pygame.draw.line(screen, (255, 255, 255), currentCircle.position,
+                                onScreenCircles[len(onScreenCircles)-2], width=2)
 
     pygame.display.update()
 
 '''
 TODO:
-    Draw lines to each circle
     Implement Eulerianpath algo
 '''
